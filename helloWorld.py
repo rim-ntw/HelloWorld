54e45f69a0a339f44fa73ad521961e677171ad08
rim-ntw <71330911+rim-ntw@users.noreply.github.com>
2021-01-19 08:35:27 +0000

    Add files via upload

---
+++ helloWorld.py
@@ -0,0 +1,21 @@
+
+import os
+import logging
+from os import environ
+
+# Set environment variables
+os.environ['API_USER'] = 'rimma'
+
+# Get environment variables
+USER = os.getenv('API_USER')
+
+# setting up logging
+logging.basicConfig(level=logging.INFO, format='%(message)s')
+logger = logging.getLogger()
+logger.addHandler(logging.FileHandler('test.log', 'a'))
+print = logger.info
+
+# cheking if env var is visable, if yes writtin to log
+if environ.get('USER') is not None:
+    print('env var is visable USER: ' + USER) 
+
