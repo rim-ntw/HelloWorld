ef6a2fc38e8998310d3ab03485095a4c2f6a5304
rim-ntw <71330911+rim-ntw@users.noreply.github.com>
2021-01-20 13:59:54 +0000

    Add files via upload

---
+++ terraformAppR.tf
@@ -0,0 +1,26 @@
+resource "local_file" "helloWorld" {
+    content = <<EOT
+import logging
+import os
+from os import environ
+
+
+# Set environment variables
+os.environ['API_USER'] = 'rimma'
+
+# Get environment variables
+USER = os.getenv('API_USER')
+
+# setting up logging
+logging.basicConfig(level=logging.INFO, format='%(message)s')
+logger = logging.getLogger()
+logger.addHandler(logging.FileHandler('test.log', 'a'))
+print = logger.info
+
+# cheking if env var is visable, if yes writtin to log
+if environ.get('USER') is not None:
+    print('env var is visable USER: ' + USER) 
+
+EOT
+    filename = "/Users/Rimma.Chepik/Desktop/HelloWorld/helloWorld.py"
+}
